ff6f3ff311d2fa5fa2c37a99d9dd83c477bcb2fc
Roland Bernard <rolli.bernard@gmail.com>
2022-12-04 13:32:31 +0100

    Solved day 4 puzzle

---
+++ 04.camp-cleanup/part2.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut count = 0;
+    for line in std::io::stdin().lines() {
+        let line = line.unwrap();
+        let ranges: Vec<Vec<_>> = line
+            .split(',')
+            .map(|p| {
+                p.split('-')
+                    .map(str::parse::<i64>)
+                    .map(Result::unwrap)
+                    .collect()
+            })
+            .collect();
+        if ranges[0][1] >= ranges[1][0] && ranges[0][0] <= ranges[1][1] {
+            count += 1;
+        }
+    }
+    println!("Result: {}", count);
+}
